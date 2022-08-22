210371f34a4e312b7d86357af28689bee18683ef
Mohsin ali <98125535+mohsinali681@users.noreply.github.com>
2022-08-23 02:05:05 +0500

    some new fixes

--- js/main.js
+++ js/main.js
@@ -1,123 +1,121 @@
-$(document).ready(function(){
-    $('.hero-slider').slick({
-        slidesToShow: 1,
+$(document).ready(function () {
+  $('.hero-slider').slick({
+    slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay:false,
-    dots:true,
-   nav:false,
-    });
+    autoplay: false,
+    dots: true,
+    nav: false,
+  });
 })
 
 
-$(document).ready(function(){
-    $('.media-slider').slick({
-    
-        slidesToShow: 1,
+$(document).ready(function () {
+  $('.media-slider').slick({
+
+    slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay:false,
-    dots:true,  
-    });
+    autoplay: false,
+    dots: true,
+  });
 })
 
 
-$(document).ready(function(){
-    $('.award-slider').slick({
-        slidesToShow: 3,
+$(document).ready(function () {
+  $('.award-slider').slick({
+    slidesToShow: 3,
     slidesToScroll: 1,
-    autoplay:false,
-    dots:false,
+    autoplay: false,
+    dots: false,
     prevArrow: '<span class="prev-arrow3" ><i class="fal fa-chevron-left"></i></span>',
-     nextArrow: '<span class="next-arrow3" ><i class="fal fa-chevron-right"></i></span>' 
-    });
+    nextArrow: '<span class="next-arrow3" ><i class="fal fa-chevron-right"></i></span>'
+  });
 })
 
 //change the active class
-$(document).ready(function(){
-  $('.section-bullets li a').click(function(){
-    $('.section-bullets li a.active').removeClass('active');
-    $(this).addClass('active');
-  });
+// $(document).ready(function () {
+//   $('.section-bullets li a').click(function () {
+//     $('.section-bullets li a.active').removeClass('active');
+//     $(this).addClass('active');
+//   });
 
-  // $(window).on('scroll', function() {
-  //   if($(this).scrollTop() > 3137){
-  //     $('.solution-svg img').removeClass('displayNone');
-  //     $('.solution-svg img').addClass('displayBlock');
-  //   };
-//});
+//   $(window).on('scroll', function() {
+//     if($(this).scrollTop() > 3137){
+//       $('.solution-svg img').removeClass('displayNone');
+//       $('.solution-svg img').addClass('displayBlock');
+//     };
+//   });
 
+
+// });
+
+
+
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+
+
+$('.myslider').slick({
+
+  slidesToScroll: 1,
+  arrows: false,
+  dots: false,
+  vertical: true,
+  verticalSwiping: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+  ]
 
 });
 
 
 
 
-
-function myFunction(x) {
-    x.classList.toggle("change");
-  }
-
-
-  $('.myslider').slick({
-
-    slidesToScroll: 1,
-    arrows: false,
-    dots: false,
-    vertical: true,
-    verticalSwiping: true,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-    ]
-        
-  });
-
- 
-
-
 // function for background image change state in together page
-  $('#analyze-state').hover(function(){
-    $('#section6').css('background-image', 'url("images/work-main-state.png")');
-  });
-  $('#interview-state').hover(function() {
-    $('#section6').css('background-image', 'url("images/interview-state.png")');
-  });
-  $('#choose-state').hover(function() {
-    $('#section6').css('background-image', 'url("images/choose-state.png")');
-  });
-  
-  $('#launch-state').hover(function() {
-    $('#section6').css('background-image', 'url("images/launch-state.png")');
-  });
-  $('#scale-state').hover(function() {
-    $('#section6').css('background-image', 'url("images/scale-state.png")');
-  });
-  
-  $('a').on('mouseleave', function(){
-    $('.together').removeClass("hover");
-  });
+$('#analyze-state').hover(function () {
+  $('#section6').css('background-image', 'url("images/work-main-state.png")');
+});
+$('#interview-state').hover(function () {
+  $('#section6').css('background-image', 'url("images/interview-state.png")');
+});
+$('#choose-state').hover(function () {
+  $('#section6').css('background-image', 'url("images/choose-state.png")');
+});
+
+$('#launch-state').hover(function () {
+  $('#section6').css('background-image', 'url("images/launch-state.png")');
+});
+$('#scale-state').hover(function () {
+  $('#section6').css('background-image', 'url("images/scale-state.png")');
+});
+
+$('a').on('mouseleave', function () {
+  $('.together').removeClass("hover");
+});
 // function ends
 
 // $(document).ready(function() {
@@ -131,10 +129,10 @@ function myFunction(x) {
 
 var activeNavItem = $('.nav-item');
 
-activeNavItem.click(function(){
+activeNavItem.click(function () {
   activeNavItem.removeClass('active');
-  
-  $(this).addClass('active');  
+
+  $(this).addClass('active');
 });
 
 //  ON SCROOOLLL ACTIVE BUTTON CHNAGE
@@ -175,31 +173,31 @@ activeNavItem.click(function(){
 //   function(){ 
 //     $(".nav-item.inactive").addClass('active').removeClass('inactive'); 
 //   },
-  
+
 // );
 
-  $(document).ready(function() { // DOM ready
-    // If a link has a dropdown, add sub menu toggle.
-  
-    $('nav ul li a:not(:only-child)').click(function(e) {
-      $(this).siblings('.nav-dropdown').toggle();
-      // Close one dropdown when selecting another
-      $('.nav-dropdown').not($(this).siblings()).hide();
-      e.stopPropagation();
-    });
-    // Clicking away from dropdown will remove the dropdown class
-    $('html').click(function() {
-      $('.nav-dropdown').hide();
-    });
-    // Toggle open and close nav styles on click
-    $('#nav-toggle').click(function() {
-      $('nav ul').slideToggle();
-    });
-    // Hamburger to X toggle
-    $('#nav-toggle').on('click', function() {
-      this.classList.toggle('active');
-    });
-  }); // end DOM ready
+$(document).ready(function () { // DOM ready
+  // If a link has a dropdown, add sub menu toggle.
+
+  $('nav ul li a:not(:only-child)').click(function (e) {
+    $(this).siblings('.nav-dropdown').toggle();
+    // Close one dropdown when selecting another
+    $('.nav-dropdown').not($(this).siblings()).hide();
+    e.stopPropagation();
+  });
+  // Clicking away from dropdown will remove the dropdown class
+  $('html').click(function () {
+    $('.nav-dropdown').hide();
+  });
+  // Toggle open and close nav styles on click
+  $('#nav-toggle').click(function () {
+    $('nav ul').slideToggle();
+  });
+  // Hamburger to X toggle
+  $('#nav-toggle').on('click', function () {
+    this.classList.toggle('active');
+  });
+}); // end DOM ready
 //   })
 (jQuery);
 
@@ -207,9 +205,8 @@ activeNavItem.click(function(){
 
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
-output.innerHTML = slider.value; 
+output.innerHTML = slider.value;
 
-slider.oninput = function() {
+slider.oninput = function () {
   output.innerHTML = this.value;
-} 
-
+}
